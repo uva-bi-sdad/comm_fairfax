b9f33ee643224957909538c0fc5dd5d6705cba43
Zarni Htet <zarni90@vt.edu>
2017-05-31 12:35:54 -0400

    Code working for first two codes

--- src/zarni90/acs_tut.R
+++ src/zarni90/acs_tut.R
@@ -50,7 +50,7 @@ FXCStats$zipcode <- as.numeric(FXCStats$zipcode)
 
 #Joining the two tables by ZipCode
 library(dplyr)
-M_FX_Zip %>% inner_join(FXCStats, by = "zipcode")
+M_FX_Zip %>% left_join(FXCStats, by = "zipcode")
 
 
 zip_matcher <- function(tablecode){
@@ -60,7 +60,8 @@ zip_matcher <- function(tablecode){
     FX.VA.TP<-acs.fetch(geography=FX.VA, endyear=2015, table.number=tablecode, col.name="pretty")
     #May need to modify depending on table
     FXCStats<-data.frame(estimate(FX.VA.TP), round(standard.error(FX.VA.TP),0))
-    colnames(FXCStats)<-c("Total","SE.Total")
+    #I have deleted out the colnames. We can put them up when we need to.
+    #colnames(FXCStats)<-c("Total","SE.Total")
     #Establishing Zipcode from Rownames
     FXCStats$zipcode <- row.names(FXCStats)
     FXCStats$zipcode
@@ -72,13 +73,20 @@ zip_matcher <- function(tablecode){
     M_FX_Zip <- rio::import("~/git/comm_fairfax/data/fairfaxZipcodes.txt")
     M_FX_Zip <- data.frame(M_FX_Zip)
     colnames(M_FX_Zip) <- "zipcode"
-    M_FX_Zip$zipcode <- as.numeric(M_FX_Zip$zipcode)
-    FXCStats$zipcode <- as.numeric(FXCStats$zipcode)
+    M_FX_Zip$zipcode <- as.character(M_FX_Zip$zipcode)
+    FXCStats$zipcode <- as.character(FXCStats$zipcode)
     #Joining the two tables by ZipCode
-    library(dplyr)
-    FX_Zip_Final <- M_FX_Zip %>% inner_join(FXCStats, by = "zipcode")
+    FX_Zip_Final <- M_FX_Zip %>% left_join(FXCStats, by = "zipcode")
     return (FX_Zip_Final)
 }
 
+#Population by Age Group and Sex
+pop_age_group <- zip_matcher("B01003")
+write.csv(pop_age_group, file="~/git/comm_fairfax/data/comm_fairfax/working/Pop_Age_Group.csv")
+
+#Population by Race/Ethnicity
+pop_race_ethnicity <- zip_matcher("B03002")
+head(pop_race_ethnicity)
+write.csv(pop_race_ethnicity, file = "~/git/comm_fairfax/data/comm_fairfax/working/Pop_Race_Ethnicity.csv")
 
 
