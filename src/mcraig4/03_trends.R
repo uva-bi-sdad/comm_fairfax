dd34668d5bc0523ba5fe82cbf971919d34ca4299
Craig Morton <mcraig4@vt.edu>
2017-09-27 14:11:24 -0400

    Trends work

--- src/mcraig4/03_trends.R
+++ src/mcraig4/03_trends.R
@@ -1,19 +1,24 @@
 library(rio)
 library(ggplot2)
+library(dplyr)
 
 BMI <- import('data/comm_fairfax/working/ATUS_data_working/ATUS state year files/Avg_BMI.csv')
+VA_BMI <- BMI[BMI$state == 'virginia', ]
 head(BMI)
 tail(BMI)
 
 
-for (i in 2003:2016) {
-    year_avg <- sum(BMI$value)
-    print(year_avg)
-}
+by_year <- BMI %>% group_by(year)
 
-for (i in 2003:2016) {
-    avg[i] <- mean(BMI$value[i])
-    print(avg[i])
-}
+avg <- by_year %>% summarise(
+    value = mean(value)
+)
 
-if BMI$year == 2003
+BMI_Trends <- ggplot() +
+    geom_line(data = avg, aes(year, value, color = 'National')) +
+    geom_line(data = VA_BMI, aes(year, value, color = 'VA_BMI')) +
+    labs(title = 'VA vs National BMI')
+
+jpeg('output/mcraig4/BMI_Trends.jpeg', width = 1200, height = 800, quality = 100)
+plot(BMI_Trends)
+dev.off()
