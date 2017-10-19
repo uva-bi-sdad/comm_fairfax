3d5ad79dbb865ff2268515dbc9778586757867b7
Ian Crandell <ian85@vbi.vt.edu>
2017-10-19 18:09:45 +0000

    Added regex

--- src/mcraig4/06_nhis_work.R
+++ src/mcraig4/06_nhis_work.R
@@ -18,5 +18,25 @@ myfun <- function(all_tables) {
     return(all_tables[1, 2, drop=FALSE])
 }
 
+# It turns out the problem is that one of the 'tables' is just a character vector
+sapply(all_tables, class)
+# If we just remove that one from the record then it all works fine
 
-sapply(all_tables, myfun)
+# This is a slightly cleaner way to do what myfun does
+var_descriptions = sapply(all_tables[-100], "[", 1, 2)
+
+# Some regular expression (aka regex) stuff
+# https://stackoverflow.com/questions/26809847/extract-text-between-certain-symbols-using-regular-expression-in-r
+# https://www.rstudio.com/wp-content/uploads/2016/09/RegExCheatsheet.pdf
+# This stuff is nuts! Here's an example of what we can do with it
+
+char = brthwght[10, 1]
+char
+
+# Behold the beauty
+regmatches(char, regexpr("(?<=\\/\\*)(.*)(?=\\*\\/)", char, perl = T))
+
+# There's a lot going on here. The first link is to the stack overflow that got me this information. I googled "find characters between regex in r". The second link is to a cheat sheet for R's regex capabilities.
+# The text in quotes in the regexpr function is the actual regular expression itself. (?<=_) looks ahead of '_' for stuff, (?=_) looks behind '_' for stuff, and (.*) means any text at all. The double slash \\ is an escape character you have to use if you want to use regex's 'special characters' as if they were text (so since / and * mean something programmatically, we need //\//*). So in words this says "find anything (.*) that comes after /* (?<=\\/\\*) and before */ (?=\\*\\/)
+# I'll leave it to you to loop through. You'll need to consider that these tables could have different lengths. Also a good chance it'll break on some special case, so we can troublshoot if that happens.
+
