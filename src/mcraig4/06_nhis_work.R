6ef13b27b36b34fa15fcaf9a84daba54861b2046
Craig Morton <mcraig4@vt.edu>
2017-10-19 15:26:33 -0400

    10/19 work. Extracting the variable name.

--- src/mcraig4/06_nhis_work.R
+++ src/mcraig4/06_nhis_work.R
@@ -1,6 +1,6 @@
-install.packages("docxtractr")
-
+#install.packages("docxtractr")
 library(docxtractr)
+library(stringr)
 
 NHIS <- read_docx('src/mcraig4/2014-nhis-sas-codebook.docx')
 docx_tbl_count(NHIS)
@@ -23,7 +23,8 @@ sapply(all_tables, class)
 # If we just remove that one from the record then it all works fine
 
 # This is a slightly cleaner way to do what myfun does
-var_descriptions = sapply(all_tables[-100], "[", 1, 2)
+descr = data.frame(var_descriptions = sapply(all_tables[-100], "[", 1, 2))
+#write.csv(descr, file = 'output/mcraig4/nhis_descriptions.csv')
 
 # Some regular expression (aka regex) stuff
 # https://stackoverflow.com/questions/26809847/extract-text-between-certain-symbols-using-regular-expression-in-r
@@ -40,3 +41,10 @@ regmatches(char, regexpr("(?<=\\/\\*)(.*)(?=\\*\\/)", char, perl = T))
 # The text in quotes in the regexpr function is the actual regular expression itself. (?<=_) looks ahead of '_' for stuff, (?=_) looks behind '_' for stuff, and (.*) means any text at all. The double slash \\ is an escape character you have to use if you want to use regex's 'special characters' as if they were text (so since / and * mean something programmatically, we need //\//*). So in words this says "find anything (.*) that comes after /* (?<=\\/\\*) and before */ (?=\\*\\/)
 # I'll leave it to you to loop through. You'll need to consider that these tables could have different lengths. Also a good chance it'll break on some special case, so we can troublshoot if that happens.
 
+
+variable <- str_extract_all(all_tables, "(?<=\\/\\*)(.*)(?=\\*\\/)")
+require(reshape2)
+variable$id <- rownames(variable)
+variable_df <- melt(variable)
+
+#write.csv(variable_df, 'output/mcraig4/nhis_variables_used.csv')
