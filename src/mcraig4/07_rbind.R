07dc05f445759da5ffa158ff0dedb7c91b719102
Craig Morton <mcraig4@vt.edu>
2017-10-24 17:38:37 -0400

    10/24 work.

--- src/mcraig4/07_rbind.R
+++ src/mcraig4/07_rbind.R
@@ -5,7 +5,7 @@ sixth_grade_ffxys <- read_xlsx('src/mcraig4/data/sixth_grade_ffxys.xlsx')
 older_ffxys <- read_xlsx('src/mcraig4/data/older_ffxys.xlsx')
 
 # Get rid of the first NA row
-sixth_grade_ffxys <- sixth_grade_ffxys[-1, ]
+#sixth_grade_ffxys <- sixth_grade_ffxys[-1, ]
 older_ffxys<- older_ffxys[-1, ]
 
 # See how many matches there are between the two dataframe
