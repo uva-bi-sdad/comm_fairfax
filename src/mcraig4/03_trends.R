483e5d9cf9dfa93b41d915b35986f38ad85c09a0
Craig Morton <mcraig4@vt.edu>
2017-10-31 13:11:50 -0400

    BMI Trend Graph.

--- src/mcraig4/03_trends.R
+++ src/mcraig4/03_trends.R
@@ -15,9 +15,18 @@ avg <- by_year %>% summarise(
 )
 
 BMI_Trends <- ggplot() +
-    geom_line(data = avg, aes(year, value, color = 'National')) +
-    geom_line(data = VA_BMI, aes(year, value, color = 'VA_BMI')) +
-    labs(title = 'VA vs National BMI')
+    geom_line(data = avg, aes(year, value, color = 'National'), size = 4) +
+    geom_line(data = VA_BMI, aes(year, value, color = 'VA_BMI'), size = 4) +
+    labs(title = 'VA vs National BMI', colour = "Geographical Level", x = "Year", y = "BMI Value") +
+    theme(axis.text.x = element_text(size = 16),
+          axis.text.y = element_text(size = 12),
+          title = element_text(size = 20),
+          axis.title = element_text(size = 18),
+          legend.title = element_text(size = 18),
+          legend.text = element_text(size = 16))
+
+
+plot(BMI_Trends)
 
 jpeg('output/mcraig4/BMI_Trends.jpeg', width = 1200, height = 800, quality = 100)
 plot(BMI_Trends)
