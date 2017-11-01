16fda871e134d4982b4be2c7ebca11031f58dc7e
Craig Morton <mcraig4@vt.edu>
2017-11-01 14:19:13 -0400

    Added recode from car function, and rearranged code.

--- src/mcraig4/08_activities_by_hours.R
+++ src/mcraig4/08_activities_by_hours.R
@@ -1,68 +1,42 @@
-# The car package has a function to recode values
-install.packages("car")
 library(car)
+
+
+# Pull in the two survey datasets
 ys_older_2016 <- read.csv('data/comm_fairfax/original/youth survey data/2016_8_10_12_Youth_Survey.csv')
 ys_younger_2016 <- read.csv('data/comm_fairfax/original/youth survey data/2016_6_Youth_Survey.csv')
 
+
+# Subset the data to get C11,12,13,14 and H1,2 and Sleep for eighth grade (H20)
 older_c_var <- ys_older_2016[, 38:41]
 older_h_var <- ys_older_2016[, 33:34]
 older_sleep_var <- ys_older_2016[, 159:160]
 younger_c_var <- ys_younger_2016[, 44:47]
 younger_h_var <- ys_younger_2016[, 48:49]
 
-# older_c_var[older_c_var == 1] <- 0
-# older_c_var[older_c_var == 2] <- 0.5
-# older_c_var[older_c_var == 3] <- 0.75
-# older_c_var[older_c_var == 4] <- 1
-# older_c_var[older_c_var == 5] <- 2
-# older_c_var[older_c_var == 6] <- 3
 
-older_c_var <- apply(older_c_var, 2, function(x) {x <- recode(x,"1 = 0; 2 = .5; 3 = .75; 4 = 1; 5 = 2; 6 = 3"); x})
+# Using the car library (recode function), recode the values in an automated way
+older_c_var <- data.frame(apply(older_c_var, 2, function(x) {x <- recode(x,"1 = 0; 2 = .5; 3 = .75; 4 = 1; 5 = 2; 6 = 3"); x}))
+older_h_var <- data.frame(apply(older_h_var, 2, function(x) {x <- recode(x, "1=0;2=0.75;3=1;4=2;5=3;6=4;7=5"); x}))
+older_sleep_var <- data.frame(apply(older_sleep_var, 2, function(x) {x <- recode(x,"1 = 4; 2 = 5; 3 = 6; 4 = 7; 5 = 8; 6 = 9; 7 = 10"); x}))
+older_sleep_var <- data.frame(older_sleep_var[, 1])
 
-older_h_var[older_h_var == 1] <- 0
-older_h_var[older_h_var == 2] <- 0.75
-older_h_var[older_h_var == 3] <- 1
-older_h_var[older_h_var == 4] <- 2
-older_h_var[older_h_var == 5] <- 3
-older_h_var[older_h_var == 6] <- 4
-older_h_var[older_h_var == 7] <- 5
 
-older_sleep_var[older_sleep_var == 1] <- 4
-older_sleep_var[older_sleep_var == 2] <- 5
-older_sleep_var[older_sleep_var == 3] <- 6
-older_sleep_var[older_sleep_var == 4] <- 7
-older_sleep_var[older_sleep_var == 5] <- 8
-older_sleep_var[older_sleep_var == 6] <- 9
-older_sleep_var[older_sleep_var == 7] <- 10
+# Cbind all the above dataframes
+older_activity_var <- cbind(older_c_var, older_h_var, older_sleep_var)
 
-older_activity_var <- cbind(older_c_var, older_h_var)
 
+# Recode the variables from the 6th grade survey
+younger_c_var <- data.frame(apply(younger_c_var, 2, function(x) {x <- recode(x, "1=0;2=0.5;3=0.75;4=1;5=2;6=3"); x}))
+younger_h_var <- data.frame(apply(younger_h_var, 2, function(x) {x <- recode(x, "1=0;2=0.75;3=1;4=2;5=3;6=4;7=5"); x}))
+
+
+# Cbind the two above datasets
+younger_activity_var <- cbind(younger_c_var, younger_h_var)
+
+
+# Get the mean and standard deviation from the two above survey datasets
 colMeans(older_activity_var, na.rm = TRUE)
 sapply(older_activity_var, sd, na.rm = TRUE)
 
-younger_c_var[younger_c_var == 1] <- 0
-younger_c_var[younger_c_var == 2] <- 0.5
-younger_c_var[younger_c_var == 3] <- 0.75
-younger_c_var[younger_c_var == 4] <- 1
-younger_c_var[younger_c_var == 5] <- 2
-younger_c_var[younger_c_var == 6] <- 3
-
-younger_h_var[younger_h_var == 1] <- 0
-younger_h_var[younger_h_var == 2] <- 0.45
-younger_h_var[younger_h_var == 3] <- 1
-younger_h_var[younger_h_var == 4] <- 2
-younger_h_var[younger_h_var == 5] <- 3
-younger_h_var[younger_h_var == 6] <- 4
-younger_h_var[younger_h_var == 7] <- 5
-
-younger_activity_var <- cbind(younger_c_var, younger_h_var)
-
 colMeans(younger_activity_var, na.rm = TRUE)
 sapply(younger_activity_var, sd, na.rm = TRUE)
-
-## Want to learn how to automate this process
-# df <- {}
-# for (i in ) {
-#     older_c_var[older_c_var == i] <- c(0, 0.5, 0.75, 1, 2, 3)
-#
-# }
