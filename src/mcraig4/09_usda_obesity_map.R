1554ae9d59368f642b39af25cac3643bfdab6912
Craig Morton <mcraig4@vt.edu>
2017-11-07 15:45:16 -0500

    Fixing the merge issue where there are counties with the same name in multiple states. Work in progress, but it is almost finsihed.

--- src/mcraig4/09_usda_obesity_map.R
+++ src/mcraig4/09_usda_obesity_map.R
@@ -1,17 +1,29 @@
 library(ggplot2)
 library(maps)
 library(dplyr)
+library(stringr)
 
 county_obesity <- read.csv('data/comm_fairfax/working/mcraig4/health_usda.csv', stringsAsFactors = FALSE)
 counties <- map_data("county")
+data("state.fips")
+
+# Minor changes to get county_obesity into usable form
 county_obesity$County[1803] = "dona ana"
 county_obesity$County = tolower(county_obesity$County)
 
-obesity.map <- left_join(counties, county_obesity, by = c("subregion" = "County"))
+# Join counties and state.fips, so you can get the FIPS code for each state in this dataframe
+counties_st_ab <- left_join(counties, state.fips, by = c("region" = "polyname"))
+# NEED TO CHANGE POLYNAME LEVELS
+# NEW YORK IS newyork:main
+
+obesity.map <- merge(counties_st_ab, county_obesity, by.x = c("subregion", "abb"), by.y = c("County", "State"))
 
 MAP1<-(ggplot() +
            geom_polygon(data=obesity.map, aes(x=long, y=lat, group=group, fill=PCT_OBESE_ADULTS13), color="black", size=.25) +
-           geom_polygon(data=counties, aes(x=long, y=lat, group=group), fill=NA, color="gray48", size=.25))
+           geom_polygon(data=counties, aes(x=long, y=lat, group=group), fill=NA, color="gray48", size=.25) +
+           scale_fill_gradient(low = 'lightskyblue1', high = 'mediumblue'))
 
 plot(MAP1)
-plot(us_counties)
+
+# Make an additional VA Map
+va_obesity <- subset(county_obesity, county_obesity$State == "VA")
