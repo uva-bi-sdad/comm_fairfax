d08055c49cf17001df297fc159c6e6deb18ce2d2
Craig Morton <mcraig4@vt.edu>
2017-11-08 14:44:34 -0500

    Lines 24-27 are causing me issues.

--- src/mcraig4/09_usda_obesity_map.R
+++ src/mcraig4/09_usda_obesity_map.R
@@ -1,7 +1,6 @@
 library(ggplot2)
 library(maps)
 library(dplyr)
-library(stringr)
 
 county_obesity <- read.csv('data/comm_fairfax/working/mcraig4/health_usda.csv', stringsAsFactors = FALSE)
 counties <- map_data("county")
@@ -10,13 +9,23 @@ data("state.fips")
 # Minor changes to get county_obesity into usable form
 county_obesity$County[1803] = "dona ana"
 county_obesity$County = tolower(county_obesity$County)
+# Minor changes to get state.fips into usable form
+state.fips$polyname[20:22] = "massachusetts"
+state.fips$polyname[23:24] = "michigan"
+state.fips$polyname[34:37] = "new york"
+state.fips$polyname[38:40] = "north carolina"
+state.fips$polyname[53:55] = "virginia"
+state.fips$polyname[56:60] = "washington"
 
-# Join counties and state.fips, so you can get the FIPS code for each state in this dataframe
+# Join counties and state.fips
+# Need the state abbreviation from the stat.fips frame
 counties_st_ab <- left_join(counties, state.fips, by = c("region" = "polyname"))
-# NEED TO CHANGE POLYNAME LEVELS
-# NEW YORK IS newyork:main
 
+# Merge on two variables so counties with the same name correspond to the right state
+# This data is not usable for ggplot2
+# Need to figure out how to get it into map_data()
 obesity.map <- merge(counties_st_ab, county_obesity, by.x = c("subregion", "abb"), by.y = c("County", "State"))
+
 
 MAP1<-(ggplot() +
            geom_polygon(data=obesity.map, aes(x=long, y=lat, group=group, fill=PCT_OBESE_ADULTS13), color="black", size=.25) +
