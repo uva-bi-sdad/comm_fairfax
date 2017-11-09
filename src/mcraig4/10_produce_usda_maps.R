550d9b701b7eb7e631d442492c285595b843989b
Craig Morton <mcraig4@vt.edu>
2017-11-09 16:13:43 -0500

    Updated 09 and 10 files, and deleted 12_original_10_file. Need the wrapper function from Ian. Almost ready to produce all maps.

--- src/mcraig4/10_produce_usda_maps.R
+++ src/mcraig4/10_produce_usda_maps.R
@@ -7,13 +7,13 @@
 ##                                                                                ##
 ## Assign it a name                                                               ##
 ##                                                                                ##
-## Change lines 29-34 to get your dataframe into usable format                    ##
+## Change lines 30-35 to get your dataframe into usable format                    ##
 ##                                                                                ##
-## Change to the name of your_csv in line 53 of the merge, and also change the by ##
+## Change to the name of your_csv in line 54 of the merge, and also change the by ##
 ## the the appropriate column names                                               ##
 ##                                                                                ##
-## In ggplot at the bottom (lines 57 & 69), change fill to the variable you want  ##
-##                                                                                ##
+## In ggplot at the bottom (lines 58 & 70), change fill to the variable you want  ##
+## Ian is going to write a wrapper function for this                              ##
 ## Run the code, and it everything else should be automatic                       ##
 ####################################################################################
 
@@ -24,6 +24,7 @@ library(dplyr)
 your_csv <- read.csv('data/comm_fairfax/working/usda_data/health_usda.csv', stringsAsFactors = FALSE)
 counties <- map_data("county")
 data("state.fips")
+states <- map_data('state')
 
 # Minor changes to get county_obesity into usable form
 your_csv$County[1803] = "dona ana"
@@ -49,23 +50,24 @@ counties$subregion = gsub("\\s",'', counties$subregion)
 counties_st_ab <- left_join(counties, state.fips, by = c("region" = "polyname"))
 
 # Merge on two variables so counties with the same name correspond to the right state
-# Got line 28 from looking through ?map_data
+# Got line 54 from looking through ?map_data
 obesity.map <- merge(counties_st_ab, your_csv, by.x = c("subregion", "abb"), by.y = c("County", "State"))
 obesity.map <- obesity.map[order(obesity.map$order), ]
 
 MAP1<-(ggplot() +
            geom_polygon(data=obesity.map, aes(x=long, y=lat, group=group, fill=PCT_OBESE_ADULTS13), color="black", size=.25) +
            geom_polygon(data=counties, aes(x=long, y=lat, group=group), fill=NA, color="gray48", size=.25) +
-           scale_fill_gradient(low = "#E69F00", high = "#56B4E9", limits = range(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), breaks = round(seq(min(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), max(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), length = 5), 1)) +
-           coord_fixed(1.3)) +
-    labs(title = "Obesity Rates for 2013") +
-    theme(axis.title=element_blank(),
-          axis.text=element_blank(),
-          axis.ticks=element_blank(),
-          text = element_text(size = 20))
+           geom_polygon(data=states, aes(x=long, y=lat, group=group), fill=NA, color="black", size=0.15) +
+           scale_fill_gradient(low = "#56B4E9", high = "#E69F00", limits = range(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), breaks = round(seq(min(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), max(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), length = 5), 1)) +
+           coord_fixed(1.3) +
+           labs(title = "Obesity Rates for 2013") +
+           theme(axis.title=element_blank(),
+                 axis.text=element_blank(),
+                 axis.ticks=element_blank(),
+                 panel.background = element_blank(),
+                 text = element_text(size = 20)))
 
 plot(MAP1)
-
 
 # Make an additional VA Map
 va_obesity <- subset(obesity.map, obesity.map$abb == "VA")
@@ -74,12 +76,13 @@ VA_counties <- subset(counties, counties$region == "virginia")
 VA_MAP<-(ggplot() +
              geom_polygon(data=va_obesity, aes(x=long, y=lat, group=group, fill=PCT_OBESE_ADULTS13), color="black", size=.25) +
              geom_polygon(data=VA_counties, aes(x=long, y=lat, group=group), fill=NA, color="gray48", size=.25) +
-             scale_fill_gradient(low = "#E69F00", high = "#56B4E9", limits = range(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), breaks = round(seq(min(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), max(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), length = 5), 1)) +
-             coord_fixed(1.3)) +
-    labs(title = "Obesity Rates for 2013") +
-    theme(axis.title=element_blank(),
-          axis.text=element_blank(),
-          axis.ticks=element_blank(),
-          text = element_text(size = 20))
+             scale_fill_gradient(low = "#56B4E9", high = "#E69F00", limits = range(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), breaks = round(seq(min(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), max(obesity.map$PCT_OBESE_ADULTS13, na.rm = T), length = 5), 1)) +
+             coord_fixed(1.3) +
+             labs(title = "Obesity Rates for 2013") +
+             theme(axis.title=element_blank(),
+                   axis.text=element_blank(),
+                   axis.ticks=element_blank(),
+                   panel.background = element_blank(),
+                   text = element_text(size = 20)))
 
 plot(VA_MAP)
